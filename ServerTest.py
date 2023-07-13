1a28e186866617a6042c3cd0de9613018288918b
themadmonkeyman <benbrittle@icloud.com>
2023-07-13 20:28:27 +0100

    Testing commits

---
+++ ServerTest.py
@@ -0,0 +1,18 @@
+import socket
+import sys
+
+HOST = '127.0.0.1'
+PORT = 3450
+
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        sys.stdout.write(conn, addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
+sys.stdout.write('done')
