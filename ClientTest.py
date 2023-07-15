36cbf538cc3fd305955f964896c4d675d07c1f82
themadmonkeyman <benbrittle@icloud.com>
2023-07-15 16:42:40 +0100

    Testing commits

--- ClientTest.py
+++ ClientTest.py
@@ -1,12 +1,12 @@
 import socket
+import keyboard as keys
 
 HOST = '127.0.0.1'
 PORT = 3450
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # connecting to server
+s.connect((HOST, PORT))
 
-with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
 
+data = s.recv(1024)  # checking to see if data gets received
 print(f'Received {data!r}')
